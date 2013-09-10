2416ea2da57cdf25efde05f9fc4708267653caa0
Simon de la Court <info@simondelacourt.nl>
2013-09-10 13:21:41 +0200

    added template stuff, working on settings

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,2 +1,13 @@
 class Admin::SettingsController < ApplicationController
+  layout 'admin'
+  before_filter :authenticate_user!
+  def index
+    @settings = Setting.find(:all)
+  end
+  def edit
+    @setting = Setting.find(params[:id])
+  end
+  def update
+    
+  end
 end
--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -1,2 +1,8 @@
 class Admin::TemplatesController < ApplicationController
+  layout 'admin'
+  before_filter :authenticate_user!
+  def index
+    @templates = Template.find(:all)
+    
+  end
 end
